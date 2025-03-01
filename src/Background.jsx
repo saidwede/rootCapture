c0aedccd2753b4745779cdc08b564459817c16fa
SAID BIO WEDE <saidbiowede@gmail.com>
2025-03-01 01:31:14 +0100

    .

--- src/Background.jsx
+++ src/Background.jsx
@@ -1,53 +1,81 @@
 export default function Background (){
     return(
-        <div className="background">
-            <div className="hdigits">
-                <HDigits />
-                <HDigits />
-                <HDigits />
-                <HDigits />
+        <>
+            <div className="background">
+                <div className="glow"></div>
+                <img className="circles" src="/img/circles-bg.png" alt="" />
+                <div className="halo"></div>
             </div>
-            <div className="vdigits">
-                <VDigits />
-                <VDigits />
-                <VDigits />
-                <VDigits />
-            </div>
-            <div className="glow"></div>
-        </div>
-    )
-}
+            <div className="ovelay">
+                <img
+                    style={{
+                        height: '50px',
+                        width: '50px',
+                        top: '30%',
+                        left: '30%'
+                    }} 
+                src="/img/Elemnts8.png" alt="" />
+                <img
+                    style={{
+                        height: '50px',
+                        width: '50px',
+                        top: '30%',
+                        right: '30%'
+                    }} 
+                src="/img/Elemnts8.png" alt="" />
+                <img
+                    style={{
+                        height: '50px',
+                        width: '50px',
+                        bottom: '30%',
+                        right: '30%'
+                    }} 
+                src="/img/Elemnts8.png" alt="" />
+                <img
+                    style={{
+                        height: '50px',
+                        width: '50px',
+                        top: '40%',
+                        right: '47%'
+                    }} 
+                src="/img/Elemnts4.png" alt="" />
+                
 
-function HDigits(){
-    return (
-        <>
-            <span>0</span>
-            <span>10</span>
-            <span>20</span>
-            <span>30</span>
-            <span>40</span>
-            <span>50</span>
-            <span>60</span>
-            <span>70</span>
-            <span>80</span>
-            <span>90</span>
-            <span>00</span>
-        </>
-    )
-}
-function VDigits(){
-    return (
-        <>
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
-            <span>4</span>
-            <span>5</span>
-            <span>6</span>
-            <span>7</span>
-            <span>8</span>
-            <span>9</span>
-            <span>0</span>
+
+                <img
+                    style={{
+                        height: '50px',
+                        width: '50px',
+                        top: '30%',
+                        right: '42%'
+                    }} 
+                src="/img/Elemnts2.png" alt="" />
+                <img
+                    style={{
+                        height: '50px',
+                        width: '50px',
+                        bottom: '30%',
+                        left: '30%'
+                    }} 
+                src="/img/Elemnts4.png" alt="" />
+                <img
+                    style={{
+                        height: '50px',
+                        width: '50px',
+                        bottom: '45%',
+                        right: '30%'
+                    }} 
+                src="/img/Elemnts5.png" alt="" />
+                <img
+                    style={{
+                        height: '50px',
+                        width: '50px',
+                        bottom: '45%',
+                        left: '40%'
+                    }} 
+                src="/img/Elemnts6.png" alt="" />
+                
+            </div>
         </>
     )
 }
